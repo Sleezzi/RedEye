ea906f98c874687c451a1cd4822de313b18a6179
Sleezzi <ilan.dfp91@gmail.com>
2024-01-23 12:31:05 +0100

    First commit

---
+++ fadeup.js
@@ -0,0 +1,11 @@
+function handkeScroll() {
+    document.querySelectorAll(".fade-up").forEach(element => {
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('active');
+        } else {
+            element.classList.remove('active');
+        }
+    });
+}
+handkeScroll();
+window.addEventListener('scroll', handkeScroll);
