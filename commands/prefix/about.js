aacdaccff2ea6ed7de7cdba1e595755b48e81df3
Sleezzi <ilan.dfp91@gmail.com>
2024-01-29 13:13:58 +0100

    .

--- commands/prefix/about.js
+++ commands/prefix/about.js
@@ -15,7 +15,7 @@ module.exports = {
                     { name: "<:nametag:1200757678104915978> __**Server name:**__", value: `**\`${message.guild.name}\`**`},
                     { name: ":book: __**Server description:**__", value: `**\`${message.guild.description}\`**`},
                     { name: "<:ID:1200784630865985598> __**ID:**__", value: `\`${message.guild.id}\``},
-                    { name: "<:owner:1200816888364683396> __**Owner:**__", value: `${message.guild.members.cache.find()} (id: ${})`},
+                    { name: "<:owner:1200816888364683396> __**Owner:**__", value: `${guild.members.cache.find(member => member.id === guild.ownerId).user.username} (id: ${guild.members.cache.find(member => member.id === guild.ownerId).id})`},
                     { name: ":inbox_tray: __**You have been a member since:**__", value: `<t:${Math.floor(message.member.joinedTimestamp / 1000)}:d> (<t:${Math.floor(message.member.joinedTimestamp / 1000)}:R>)`},
                     { name: ":computer: __**Server created:**__", value: `<t:${Math.floor(message.guild.createdTimestamp / 1000)}:d> (<t:${Math.floor(message.guild.createdTimestamp / 1000)}:R>)`},
                     { name: ":hourglass: __**Date:**__", value: `<t:${Math.floor(message.createdTimestamp / 1000)}:d> (<t:${Math.floor(message.createdTimestamp / 1000)}:R>)`},
