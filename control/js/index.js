6a826a4cb876fdc2f4a634cd07ad4fe53a89f76e
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2024-01-31 17:16:07 +0100

    Add files via upload

--- control/js/index.js
+++ control/js/index.js
@@ -1,5 +1,4 @@
 const data = JSON.parse(localStorage.getItem("token"));
-if (!data) window.location.href = "../thanks";
 // const firebase = require("firebase");
 // require('firebase/database');
 
@@ -19,18 +18,13 @@ if (!data) window.location.href = "../thanks";
 async function withServer(id) {
     document.querySelector("#index > #withoutGuild").style.display = "none";
     document.querySelector("#index > #withGuild").style.display = "flex";
-    let response = await fetch(`https://discord.com/api/guilds/${id}/preview`, {
-		headers: {
-		    authorization: `${data.type} ${data.token}`,
-		},
-	});
-    if (response.status !== 200) return;
-    response = await response.json();
-    console.log(response);
+    document.querySelector("#index #login button[type=\"submit\"]").onclick = () => {
+        console.log(document.querySelector("#index #login input#password").value);
+    }
 }
 
 async function withoutServer() {
-    let response = await fetch('https://discord.com/api/users/@me/guilds?limit=200&with_counts=true', {
+    let response = await fetch('https://discord.com/api/v10/users/@me/guilds?limit=200&with_counts=true', {
 		headers: {
 		    authorization: `${data.type} ${data.token}`,
 		},
@@ -44,21 +38,22 @@ async function withoutServer() {
         card.href = `./?guild=${server.id}`
         card.classList.add("guild");
         card.setAttribute("guildId", server.id);
-        card.innerHTML = `<div class="banner"></div>
+        card.innerHTML = `
+        <div class="banner"></div>
         <div>
-            <div>
-                ${(server.icon ? `<img class="avatar" src="https://cdn.discordapp.com/icons/${server.id}/${server.icon}.png" alt="${server.name}"></img>` : `<h3 class="avatar">${server.name.split(" ").map(letter => letter.slice(0, 1)).join("")}</h3>`)}
+            ${(server.icon ? `<img class="avatar" src="https://cdn.discordapp.com/icons/${server.id}/${server.icon}.png" alt="${server.name}"></img>` : `<h3 class="avatar">${server.name.split(" ").map(letter => letter.slice(0, 1)).join("").toUpperCase()}</h3>`)}
+            <div class="rank" style="background: url(../img/${(server.owner ? "owner" : (server.permissions === 536211447 ? "mod" : "member"))}.png) center/cover no-repeat #222" height="100%">
+                <aria-label>Server ${(server.owner ? "Owner" : (server.permissions === 536211447 ? "Mod" : "Member"))}</aria-label>
             </div>
-                <div class="rank"><img src="../img/${(server.owner ? "owner" : (server.permissions === 536211447 ? "mod" : "member"))}.png" alt="${(server.owner ? "Owner" : (server.permissions === 536211447 ? "Mod" : "Member"))}" height="100%"></div>
             </div>
-            <h4 class="name">${(server.name.length > 17 ? `${server.name.slice(0, 14)}...` : server.name)}</h4>
+            <h4 class="name">${(server.name.length > 16 ? `${server.name.slice(0, 10)}...` : server.name)}</h4>
         </div>`;
         document.querySelector("#index > #withoutGuild > #guildContainer").appendChild(card);
     });
-    document.querySelector("#index > #withoutGuild > h2").innerText = `You are on ${response.length} server${(response.length > 1 ? "s" : "")}`;
+    document.querySelector("#index > #withoutGuild > h2").innerText = `You are on ${response.length} server${(response.length > 1 ? "s" : "")}, but you can only moderate ${response.filter(server => server.owner || server.permissions === 536211447).length} of them`;
 }
-
 (async () => {
+    if (!data || !data.token || !data.type) return window.location.href = "../thanks";
     const params = new URLSearchParams(window.location.search);
     if (params.get("guild")) {
         withServer(params.get("guild"));
