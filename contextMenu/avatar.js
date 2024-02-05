8e2d4c53c1dfeb6d63487c872029bf44dc6b3598
Sleezzi <ilan.dfp91@gmail.com>
2024-02-05 11:32:16 +0100

    Commande de context avatar réactivé et mis dans un try catch

--- contextMenu/avatar.js
+++ contextMenu/avatar.js
@@ -10,14 +10,14 @@ module.exports = {
         dm_permission: undefined
     },
     async execute(interaction, client, Discord) {
-        console.log(interaction);
-        return;
-        const member = interaction.guild.members.cache.get(interaction.targetId);
-        if (!member) {
-            interaction.deleteReply().then(() => interaction.followUp({ content: "Unable to find the avatar of user", ephemeral: true }));
-            return;
-        }
-        
-        interaction.deleteReply().then(() => interaction.followUp({ content: `[This](${member.user.avatarURL()}) is the profile picture of ${member.user.username}`, ephemeral: true }));
+        try {
+            const member = interaction.guild.members.cache.get(interaction.targetId);
+            if (!member) {
+                interaction.deleteReply().then(() => interaction.followUp({ content: "Unable to find the avatar of user", ephemeral: true }));
+                return;
+            }
+            
+            interaction.deleteReply().then(() => interaction.followUp({ content: `[This](${member.user.avatarURL()}) is the profile picture of ${member.user.username}`, ephemeral: true }));
+        } catch (err) { console.error(err); }
     }
 }
