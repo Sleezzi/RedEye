b0a768523a1e7257a24d42a61f7287d825a2cd7e
Sleezzi <ilan.dfp91@gmail.com>
2024-03-25 14:45:31 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Index from "./components/Index";
 import DocsIndex from "./components/docs/Content/Index";
-import CodeRecovery from "./components/docs/Content/getting-started/code-recovery";
-import InstallingNodeJsModules from "./components/docs/Content/getting-started/installing-node-js-modules";
-import ConfigJson from "./components/docs/Content/getting-started/config-json";
-import Launch from "./components/docs/Content/getting-started/launch";
+import DocsCodeRecovery from "./components/docs/Content/getting-started/code-recovery";
+import DocsInstallingNodeJsModules from "./components/docs/Content/getting-started/installing-node-js-modules";
+import DocsConfigJson from "./components/docs/Content/getting-started/config-json";
+import DocsLaunch from "./components/docs/Content/getting-started/launch";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -13,10 +13,10 @@ function App() {
         <Routes>
             <Route index element={<Index />} />
             <Route path="/docs" exact element={<DocsIndex/>} />
-            <Route path="/docs/gettings-started/get-code" element={<CodeRecovery/>} />
-            <Route path="/docs/gettings-started/download-modules" element={<InstallingNodeJsModules/>} />
-            <Route path="/docs/gettings-started/config.json" element={<ConfigJson/>} />
-            <Route path="/docs/gettings-started/launch" element={<Launch/>} />
+            <Route path="/docs/gettings-started/get-code" element={<DocsCodeRecovery/>} />
+            <Route path="/docs/gettings-started/download-modules" element={<DocsInstallingNodeJsModules/>} />
+            <Route path="/docs/gettings-started/config.json" element={<DocsConfigJson/>} />
+            <Route path="/docs/gettings-started/launch" element={<DocsLaunch/>} />
         </Routes>
     );
 }
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react"
+
+import "../cdn/css/index.css";
+import { Link } from "react-router-dom";
+
 export default function Index() {
+    useEffect(() => {
+        document.title = "RedEye Bot";
+        document.body.onscroll = () => {
+            console.log("scroll");
+            document.querySelectorAll(".fade-up").forEach(element => {
+                if (element.getBoundingClientRect().top < window.innerHeight) {
+                    element.classList.add('active');
+                } else {
+                    element.classList.remove('active');
+                }
+            });
+            document.querySelectorAll(".fade-down").forEach(element => {
+                if (element.getBoundingClientRect().top < window.innerHeight) {
+                    element.classList.add('active');
+                } else {
+                    element.classList.remove('active');
+                }
+            });
+        };
+    }, []);
+
     return (
         <>
-            Salut
+            <Link to="/docs">Docs</Link>
         </>
     )
 }
